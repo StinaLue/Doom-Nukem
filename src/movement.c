3b48789cf37da3ee9e0c5a6a3a8b2553f32794b4
Paxiuz <pacomehaydont@gmail.com>
2019-12-05 17:10:26 +0100

    reworked collision to work with distance

--- src/movement.c
+++ src/movement.c
@@ -6,17 +6,20 @@
 /*   By: phaydont <phaydont@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/28 14:30:58 by phaydont          #+#    #+#             */
-/*   Updated: 2019/12/05 16:40:46 by phaydont         ###   ########.fr       */
+/*   Updated: 2019/12/05 17:06:57 by phaydont         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "doom.h"
 
-int		check_side_wall(double pos_x, double pos_y, t_vecdb start_wall, t_vecdb end_wall)
+int		check_side_wall(t_vecdb c, t_vecdb a, t_vecdb b)
 {
-	double det = (start_wall.x - end_wall.x) * (start_wall.y - pos_y) - (start_wall.x - pos_x) * (start_wall.y - end_wall.y);
+	double dist;
+	//double det = (start_wall.x - end_wall.x) * (start_wall.y - pos_y) - (start_wall.x - pos_x) * (start_wall.y - end_wall.y);
 	//printf("%f\n", det);
-	if (det < 0) // the lower this number, the bigger the distance between player and collision will be
+	dist = (c.x - a.x) * (a.y - b.y) + (c.y - a.y) * (b.x - a.x);
+	dist /= sqrt((a.y - b.y) * (a.y - b.y) + (b.x - a.x) * (b.x- a.x));
+	if (dist < -3) // the lower this number, the bigger the distance between player and collision will be
 		return (0);
 	else
 		return (1);
@@ -24,11 +27,15 @@ int		check_side_wall(double pos_x, double pos_y, t_vecdb start_wall, t_vecdb end
 
 int 	check_collision(double pos_x, double pos_y, t_wall *walls)
 {
+	t_vecdb point;
 	int i;
+
+	point.x = pos_x;
+	point.y = pos_y;
 	i = 0;
 	while(i < NB_WALLS)//loop through each wall
 	{
-		if (check_side_wall(pos_x, pos_y, walls[i].start_wall, walls[i].end_wall))
+		if (check_side_wall(point, walls[i].start_wall, walls[i].end_wall))
 			return (1);
 		i++;
 	}
