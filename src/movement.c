80e3203ba40d6060ae5fb4c215d649ac3414b2db
Paxiuz <pacomehaydont@gmail.com>
2019-12-05 20:21:10 +0100

    cleaned up the collision functions and renamed them, remove the separate x and y collision check to make it easier to implement an actual vector collision system

--- src/movement.c
+++ src/movement.c
@@ -6,40 +6,45 @@
 /*   By: phaydont <phaydont@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/28 14:30:58 by phaydont          #+#    #+#             */
-/*   Updated: 2019/12/05 17:16:26 by phaydont         ###   ########.fr       */
+/*   Updated: 2019/12/05 20:17:39 by phaydont         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "doom.h"
 
-int		check_side_wall(t_vecdb c, t_vecdb b, t_vecdb a)
+int		collision(t_vecdb point, t_vecdb b, t_vecdb a, double distance)
 {
 	double dist;
-	//double det = (start_wall.x - end_wall.x) * (start_wall.y - pos_y) - (start_wall.x - pos_x) * (start_wall.y - end_wall.y);
-	//printf("%f\n", det);
-	dist = (c.x - a.x) * (a.y - b.y) + (c.y - a.y) * (b.x - a.x);
-	dist /= sqrt((a.y - b.y) * (a.y - b.y) + (b.x - a.x) * (b.x- a.x));
-	if (dist < 3) // the lower this number, the bigger the distance between player and collision will be
-		return (0);
-	else
+
+	dist = (point.x - a.x) * (a.y - b.y) + (point.y - a.y) * (b.x - a.x);
+	printf("wat:%f\n", dist);
+	dist /= sqrt((a.y - b.y) * (a.y - b.y) + (b.x - a.x) * (b.x - a.x));
+	printf("mur dist:%f\n", dist);
+	if (dist < distance)
 		return (1);
+	else
+		return (0);
 }
 
-int 	check_collision(double pos_x, double pos_y, t_wall *walls)
+int		try_move(t_vecdb position, t_vecdb move, t_wall *walls)
 {
-	t_vecdb point;
-	int i;
+	int		i;
 
-	point.x = pos_x;
-	point.y = pos_y;
+	position.y += move.y;
+	position.x += move.x;
 	i = 0;
-	while(i < NB_WALLS)//loop through each wall
+
+	while (i < NB_WALLS)
 	{
-		if (check_side_wall(point, walls[i].start_wall, walls[i].end_wall))
-			return (1);
+		//can change to this decide wich walls to check collision
+		if (collision(position, walls[i].start_wall, walls[i].end_wall, 1.0))
+		{
+			//can change this to calculate actual new velocity
+			return (0);
+		}
 		i++;
 	}
-	return (0);
+	return (1);
 }
 
 void	movement(t_player *player, t_vecdb move, t_wall *walls)
@@ -52,20 +57,17 @@ void	movement(t_player *player, t_vecdb move, t_wall *walls)
 	multvec(&move, movespeed);
 	move.x += player->inertia.x;
 	move.y += player->inertia.y;
-	//printf("==x movement==\n");
-	if (check_collision(player->pos.x, player->pos.y + move.y, walls) == 0)
+
+	if (try_move(player->pos, move, walls))
 	{
 		player->pos.y += move.y;
-		player->inertia.y = 0.9 *move.y;
-	}
-	else
-		player->inertia.y = 0;
-	//printf("==y movement==\n");
-	if (check_collision(player->pos.x + move.x, player->pos.y, walls) == 0)
-	{
 		player->pos.x += move.x;
-		player->inertia.x = 0.9 *move.x;
+		multvec(&move, 0.9);
+		player->inertia = move;
 	}
 	else
+	{
 		player->inertia.x = 0;
+		player->inertia.y = 0;
+	}
 }
