bb13f0f004cc63d41c22671ed0691309fdf9cccf
Paxiuz <pacomehaydont@gmail.com>
2020-01-10 14:41:56 +0100

    changed the default starting map for a bigger one, added comments to the linkedlists functions

--- src/init.c
+++ src/init.c
@@ -6,7 +6,7 @@
 /*   By: phaydont <phaydont@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/14 16:53:33 by sluetzen          #+#    #+#             */
-/*   Updated: 2020/01/07 16:40:12 by phaydont         ###   ########.fr       */
+/*   Updated: 2020/01/08 16:48:56 by phaydont         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,17 +61,24 @@ int		init_sdlmain(t_sdlmain *sdlmain)
 
 int		init_map(t_map *map)
 {
-	t_vecdb vec1 = {60, 20}; 
-	t_vecdb vec2 = {60, 40};
-	t_vecdb vec3 = {70, 90};
-	t_vecdb vec4 = {90, 20};
+	t_vecdb vec1 = {20, 60}; 
+	t_vecdb vec2 = {40, 80};
+	t_vecdb vec3 = {60, 80};
+	t_vecdb vec4 = {80, 60};
+	t_vecdb vec5 = {80, 40};
+	t_vecdb vec6 = {60, 20};
+	t_vecdb vec7 = {40, 20};
+	t_vecdb vec8 = {20, 40};
 
 	add_sector_node(&map->sector_head);
-
 	create_wall_node(&map->sector_head->wall_head, vec1, vec2, 0xff0000);
 	create_wall_node(&map->sector_head->wall_head, vec2, vec3, 0xffbb00);
 	create_wall_node(&map->sector_head->wall_head, vec3, vec4, 0x00ff00);
-	create_wall_node(&map->sector_head->wall_head, vec4, vec1, 0x0088ff);
+	create_wall_node(&map->sector_head->wall_head, vec4, vec5, 0x0088ff);
+	create_wall_node(&map->sector_head->wall_head, vec5, vec6, 0xff0000);
+	create_wall_node(&map->sector_head->wall_head, vec6, vec7, 0xffbb00);
+	create_wall_node(&map->sector_head->wall_head, vec7, vec8, 0x00ff00);
+	create_wall_node(&map->sector_head->wall_head, vec8, vec1, 0x0088ff);
 
 	return (0);
 }
