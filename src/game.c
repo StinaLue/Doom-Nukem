a8759a40e9830a69d5302766dc131a63f344f526
Antoine FONCK <antoinefonck98@gmail.com>
2020-02-11 15:34:54 +0100

    added raycast uzi, ennemy die anim and started dst rect calculating

--- src/game.c
+++ src/game.c
@@ -6,7 +6,7 @@
 /*   By: afonck <afonck@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/14 16:46:18 by afonck            #+#    #+#             */
-/*   Updated: 2020/02/11 01:25:14 by afonck           ###   ########.fr       */
+/*   Updated: 2020/02/11 15:33:12 by afonck           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -161,17 +161,48 @@ int	blit_hud_faces(t_game *game)
 
 SDL_Rect find_dstrect_enemy(t_enemy *enemy, t_player *player, SDL_Surface *dest)
 {
-	SDL_Rect return_rect;
+	SDL_Rect	return_rect;
+	t_vecdb		transformed_enem_pos;
+	t_vecdb		screenpos;
+	double		fov_ratio;
+	double		top;
+	double		bot;
+	double		ybot;
 
-	return_rect.w = enemy->clip_tex.w / ((get_point_distance(player->pos, enemy->pos)) - 10);
-	return_rect.h = enemy->clip_tex.h / ((get_point_distance(player->pos, enemy->pos)) - 10);
-	return_rect.x = (dest->w / 2) - (return_rect.w / 2);//enemy->pos.x - player->pos.x;
-	return_rect.y = 0;//enemy->pos.y - player->pos.y;
+	top = (10 + player->sector->floor_height) - (player->posz + player->height);//(sector->ceiling_height - (player->posz + player->height)) * distance_ratio;
+	bot = top - 10;
+
+	fov_ratio = player->view.b.y / player->view.b.x;
+	transformed_enem_pos = vecdb_diff(enemy->pos, player->pos);
+	transformed_enem_pos = rotate2d(transformed_enem_pos, -player->angle);
+	screenpos.x = transformed_enem_pos.x / transformed_enem_pos.y;
+	screenpos.x *= fov_ratio;
+	screenpos.x *= dest->w / 2;
+	screenpos.x += dest->w / 2;
+
+	screenpos.y = top / transformed_enem_pos.y;
+	screenpos.y *= fov_ratio;
+	screenpos.y *= dest->w / 2;
+	screenpos.y = (dest->h / 2) + player->view_z + screenpos.y;
+	ybot = bot / transformed_enem_pos.y;
+	ybot *= fov_ratio;
+	ybot *= dest->w / 2;
+	ybot += (dest->h / 2) + player->view_z  + ybot;
+	//printf("screenpos enem x %f\n", screenpos.x);// * (dest->w / 2));
+	//printf("enemy x %f y %f\n", enemy->pos.x, enemy->pos.y);
+	//printf("pos x %f y %f\n", player->pos.x, player->pos.y);
+	return_rect.h = screenpos.y - ybot;// / ((get_point_distance(player->pos, enemy->pos)) - 10);
+	return_rect.w = (double)return_rect.h / enemy->clip_tex.h * (enemy->clip_tex.w);// / ((get_point_distance(player->pos, enemy->pos)) - 10);
+	return_rect.x = (screenpos.x);// - 40;//((return_rect.w - 40) / 2);//(dest->w / 2) - (return_rect.w / 2);//enemy->pos.x - player->pos.x;
+	return_rect.y = (screenpos.y);//enemy->pos.y - player->pos.y;
+	fill_pix(dest, return_rect.x, return_rect.y, 0xFFFFFF);
+	fill_pix(dest, return_rect.x + return_rect.w, return_rect.y + return_rect.h, 0xFF00FF);
 	player->anim = player->anim;
+	(void)dest;
 	return (return_rect);
 }
 
-void		walking_enemy_anim(t_enemy *enemy)
+void		enemy_walking_anim(t_enemy *enemy)
 {
 	enemy->clip_tex.y = enemy->current_frame * enemy->clip_tex.h;
 	if (enemy->current_frame == 0 && enemy->anim_timer == 0)
@@ -191,7 +222,7 @@ void		walking_enemy_anim(t_enemy *enemy)
 	}
 }
 
-void		attack_enemy_anim(t_enemy *enemy)
+void		enemy_attack_anim(t_enemy *enemy)
 {
 	enemy->clip_tex.y = enemy->clip_tex.h * (enemy->current_frame + 4);
 	if (enemy->current_frame == 0 && enemy->anim_timer == 0)
@@ -209,14 +240,43 @@ void		attack_enemy_anim(t_enemy *enemy)
 	}
 }
 
-SDL_Rect find_srcrect_enemy(t_enemy *enemy)//, t_player *player)
+void	enemy_die_anim(t_enemy *enemy, t_enemy_info *enemy_info)
 {
-	if (enemy->state == 1)
-		walking_enemy_anim(enemy);
-	else if (enemy->state == 2)
-		attack_enemy_anim(enemy);
+	if (enemy_info->which_enemy == 0)
+		enemy->clip_tex.y = enemy->clip_tex.h * 8;
+	else if (enemy_info->which_enemy == 1)
+		enemy->clip_tex.y = enemy->clip_tex.h * 9;
+	enemy->clip_tex.x = enemy->clip_tex.w * enemy->current_frame;
+	if (enemy->current_frame == 0 && enemy->anim_timer == 0)
+		enemy->anim_timer = SDL_GetTicks();
+	if ((SDL_GetTicks() - enemy->anim_timer) >= 150)
+	{
+		enemy->current_frame++;
+		enemy->anim_timer = SDL_GetTicks();
+	}
+	if (enemy->current_frame >= 7)
+		enemy->state = 3;
+}
+
+void	enemy_dead_rect(t_enemy *enemy)
+{
+	enemy->clip_tex.y = enemy->clip_tex.h * 8;
+	enemy->clip_tex.x = enemy->clip_tex.w * 6;
+}
+
+SDL_Rect find_srcrect_enemy(t_enemy *enemy, t_enemy_info *enemy_info)//, t_player *player)
+{
+	if (enemy->state == 1 && enemy->health > 0)
+		enemy_walking_anim(enemy);
+	else if (enemy->state == 2 && enemy->health > 0)
+		enemy_attack_anim(enemy);
+	else if (enemy->state == 2 && enemy->health <= 0)
+		enemy_die_anim(enemy, enemy_info);
+	else if (enemy->state == 3)
+		enemy_dead_rect(enemy);
 	else
 		enemy->anim_timer = 0;
+	//printf("enemy health %d\n", enemy->health);
 	return (enemy->clip_tex);
 }
 
@@ -228,11 +288,11 @@ int	blit_enemies(t_game *game, SDL_Surface *dest, t_map *map)
 	i = 0;
 	while (i < map->num_enemies)
 	{
-		if (game->player.anim == 1)
+		if (game->player.anim == 1 && game->enemy[i].state != 3)
 			game->enemy[i].state = 2;
-		else if (game->player.is_moving == 1)
+		else if (game->player.is_moving == 1 && game->enemy[i].state != 3)
 			game->enemy[i].state = 1;
-		game->enemy[i].clip_tex = find_srcrect_enemy(&game->enemy[i]);//, &game->player);
+		game->enemy[i].clip_tex = find_srcrect_enemy(&game->enemy[i], &map->enemy_info[i]);//, &game->player);
 		destrect = find_dstrect_enemy(&game->enemy[i], &game->player, dest);
 		if ((SDL_BlitScaled(game->enemy[i].texture, &game->enemy[i].clip_tex, dest, &destrect)) != 0)
 			return (error_return("SDL_BlitScaled error: %{r}s\n", SDL_GetError()));
@@ -301,6 +361,58 @@ int	blit_fps(t_game *game, int *itt, Uint32 *startclock)
 	return (0);
 }
 
+void	raycast_uzi(t_player *player, t_enemy *enemy, int num_enemies)
+{
+	int		i;
+	int		is_enemy_hit;
+	t_vecdb	dir_player;
+	int		nb_steps;
+	t_vecdb	step;
+
+	i = 0;
+	nb_steps = 0;
+	is_enemy_hit = 0;
+	dir_player.x = sin(player->angle) * -2 + player->pos.x;
+	dir_player.y = cos(player->angle) * 2 + player->pos.y;
+
+	step = vecdb_diff(dir_player, player->pos);
+	//printf("player x %f, y %f\n", player->pos.x, player->pos.y);
+	//printf("dir player x %f, y %f\n", dir_player.x, dir_player.y);
+	while (nb_steps < 50 && is_enemy_hit == 0)
+	{
+		i = 0;
+		while (i < num_enemies && is_enemy_hit == 0)
+		{
+			//printf("diff x == %d y == %d, step nb %d\n", (int)(dir_player.x - enemy[i].pos.x), (int)(dir_player.y - enemy[i].pos.y), nb_steps);
+			if ((int)(dir_player.x - enemy[i].pos.x) == 0 && (int)(dir_player.y - enemy[i].pos.y) == 0)
+			{
+				//printf("enemy hit !!\n");
+				enemy[i].health -= 1;
+				//printf("new health == %d for enemy %i\n", enemy[i].health, i);
+				is_enemy_hit = 1;
+			}
+			i++;
+		}
+		dir_player = vecdb_add(dir_player, step);
+		nb_steps++;
+	}
+}
+
+int	player_attack(t_enemy *enemy, t_player *player, t_map *map, t_gamesurfs *surfs)
+{
+	//printf("current frame %d\n", surfs->current_frame);
+	if (player->current_weapon == 0 && player->anim == 1 && surfs->current_frame == 3)
+	{
+		;
+		//if (player->pos)
+	}
+	else if (player->current_weapon == 1 && player->anim == 1 && surfs->current_frame == 2)
+	{
+		raycast_uzi(player, enemy, map->num_enemies);
+	}
+	return (0);
+}
+
 int game_loop(t_doom *doom)
 {
 	t_game		*game;
@@ -345,6 +457,7 @@ int game_loop(t_doom *doom)
 			return (error_return("Blit enemies error\n", NULL));
 		if (blit_weapon(game, game->surfs.perspective_view, game->player.current_weapon, doom->map.weapon_choice) != 0)//, &sdlmain->sound) != 0)
 			return (error_return("Blit weapon error\n", NULL));
+		player_attack(game->enemy, &game->player, &doom->map, &game->surfs);
 
 		if ((blit_hud_faces(game)) == 1)
 			return (error_return("error during blit_hud_faces\n", NULL));
